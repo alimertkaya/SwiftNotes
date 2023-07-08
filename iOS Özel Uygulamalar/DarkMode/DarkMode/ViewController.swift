6866ad5669a9c1ff4f32baa8c456ee9041cca536
Ali Mert Kaya <mert_k4433@hotmail.com>
2023-07-08 21:03:47 +0300

    DarkMode added

---
+++ iOS Özel Uygulamalar/DarkMode/DarkMode/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DarkMode
+//
+//  Created by Ali Mert Kaya on 8.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var changeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //
+    }
+
+
+}
+
