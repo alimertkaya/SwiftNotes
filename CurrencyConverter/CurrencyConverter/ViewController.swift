a315a2b67ce5ec2358e4f9990f5d2ef571424db9
Ali Mert Kaya <mert_k4433@hotmail.com>
2023-07-10 17:45:00 +0300

    CurrencyConverter created

---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Ali Mert Kaya on 10.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    @IBOutlet weak var xauLabel: UILabel!
+    @IBOutlet weak var btcLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func getRatesClicked(_ sender: Any) {
+    }
+    
+}
+
