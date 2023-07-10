829004d759882f2432c87e13c9952a908d8b21e3
Ali Mert Kaya <mert_k4433@hotmail.com>
2023-07-10 17:56:32 +0300

    Request&Session added

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -21,10 +21,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func getRatesClicked(_ sender: Any) {
+        
+        // 1) Request & Session
+        // 2) Response & Data
+        // 3) Parsing & JSON Serialization
+        
+        // 1) Request & Session
+        // url oluşturma
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=9ec1823c3c3b7979f76eddcb73a9c17e")
+        
+        // istek yapıcaz (session), veri-alışveri için
+        // .shared URLSession object i oluşturur, hep aynı object üzerinden gider birçok veri varken karışıklıklığı önler
+        let session = URLSession.shared
+        
+        // fonksiyonu çağırırken çıktı beklenir, bu çıktığı closure içerisinde verir
+        let task = session.dataTask(with: url!) { data, response, error in
+            // hata varsa kullanıcıya göster
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            }
+            else {
+                // 2) Response & Data
+                if data != nil {
+                    
+                }
+                
+            }
+        }
+        
     }
     
 }
-
