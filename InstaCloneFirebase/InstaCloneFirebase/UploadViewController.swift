ca487fb7b941006fa34adcfde8f265bdaf095529
Ali Mert Kaya <mert_k4433@hotmail.com>
2023-07-17 19:41:39 +0300

    uploadVC updated

--- InstaCloneFirebase/InstaCloneFirebase/UploadViewController.swift
+++ InstaCloneFirebase/InstaCloneFirebase/UploadViewController.swift
@@ -6,24 +6,78 @@
 //
 
 import UIKit
+import PhotosUI
+import FirebaseStorage
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, PHPickerViewControllerDelegate{
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentText: UITextField!
+    @IBOutlet weak var uploadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func selectImage() {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        present(picker, animated: true)
     }
-    */
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        if let itemProvider = results.first?.itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self) {
+            let previousImage = imageView.image
+            itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+                DispatchQueue.main.async {
+                    guard let self = self, let image = image as? UIImage, self.imageView.image == previousImage else  { return }
+                    self.imageView.image = image
+                }
+            }
+        }
+    }
 
+    @IBAction func uploadButtonClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        // reference i kullanarak hangi klasör ile çalışılacak, nereye kayıt olucak belirliyoruz
+        let storageReference = storage.reference()
+        
+        // Firebase media klasörüne ulaşır ya da yoksa oluşturur
+        let mediaFolder = storageReference.child("media")
+        
+        // imageView kayıt edebilmek için data ya çevirmek gerekiyor
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            
+            let imageReference = mediaFolder.child("image.jpg")
+            imageReference.putData(data, metadata: nil) { metadata, error in
+                if error != nil {
+                    print(error?.localizedDescription)
+                }
+                else {
+                    
+                    imageReference.downloadURL { url, error in
+                        
+                        if error == nil {
+                            
+                            let imageUrl = url?.absoluteString
+                            print(imageUrl)
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
 }
