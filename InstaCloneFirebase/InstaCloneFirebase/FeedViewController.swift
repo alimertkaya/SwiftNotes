ebc17465562b08e2ddfcae9991a3af15d04bf9a8
Ali Mert Kaya <mert_k4433@hotmail.com>
2023-07-24 22:30:19 +0300

    getDataFromFirestore added

--- InstaCloneFirebase/InstaCloneFirebase/FeedViewController.swift
+++ InstaCloneFirebase/InstaCloneFirebase/FeedViewController.swift
@@ -6,10 +6,15 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
 
     @IBOutlet weak var tableView: UITableView!
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,18 +23,67 @@ class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDat
         tableView.dataSource = self
 
         // Do any additional setup after loading the view.
+        
+        getDataFromFirestore()
+    }
+    
+    func getDataFromFirestore() {
+        
+        let fireStoreDatabase = Firestore.firestore()
+        /*
+        -> Date kaydettiğimiz için ek ayar gerekliydi eski sürümlerde
+        -> fireStoreDatabase ayarlarını değiştirmek için tanımlandı
+        let settings = fireStoreDatabase.settings
+        settings.timestampsInSnaphotsEnable = true
+        fireStoreDatabase.settings = settings
+         */
+        
+        fireStoreDatabase.collection("Posts").addSnapshotListener { snapshot, error in
+            if error != nil {
+                print(error?.localizedDescription)
+            }
+            else {
+                if snapshot?.isEmpty != true && snapshot != nil {
+                    // snapshot!.documents -> Database de olan bilgileri bir dictionary içerisinde alır
+                    for document in snapshot!.documents {
+                        let documentID = document.documentID
+                        
+                        // snapshot.documents veriyi String to Any olarak verir o yüzden String olarak cast etmem lazım
+                        if let postedBy = document.get("postedBy") as? String {
+                            self.userEmailArray.append(postedBy)
+                        }
+                        
+                        if let postComment = document.get("postComment") as? String {
+                            self.userCommentArray.append(postComment)
+                        }
+                        
+                        if let likes = document.get("likes") as? Int {
+                            self.likeArray.append(likes)
+                        }
+                        
+                        if let imageUrl = document.get("imageUrl") as? String {
+                            self.userImageArray.append(imageUrl)
+                        }
+                        
+                    }
+                    
+                    // tableView güncellensin
+                    self.tableView.reloadData()
+                }
+            }
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userEmailArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // FeedCell tanımlandı
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        cell.userEmailLabel.text = "test"
-        cell.likeLabel.text = "0"
-        cell.commentLabel.text = "comment"
+        cell.userEmailLabel.text = userEmailArray[indexPath.row]
+        cell.likeLabel.text = String(likeArray[indexPath.row])
+        cell.commentLabel.text = userCommentArray[indexPath.row]
         cell.userImageView.image = UIImage(named: "select.png")
         return cell
     }
