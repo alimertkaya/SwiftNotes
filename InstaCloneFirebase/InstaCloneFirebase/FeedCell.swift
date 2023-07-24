6b1af73c229d08fa17432caa8c9ff62ac693ef20
Ali Mert Kaya <mert_k4433@hotmail.com>
2023-07-24 17:33:44 +0300

    FeedViewController tableView added

---
+++ InstaCloneFirebase/InstaCloneFirebase/FeedCell.swift
@@ -0,0 +1,32 @@
+//
+//  FeedCell.swift
+//  InstaCloneFirebase
+//
+//  Created by Ali Mert Kaya on 24.07.2023.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var likeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func likeButtonClicked(_ sender: Any) {
+        
+    }
+    
+}
--- InstaCloneFirebase/InstaCloneFirebase/FeedViewController.swift
+++ InstaCloneFirebase/InstaCloneFirebase/FeedViewController.swift
@@ -7,23 +7,31 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // FeedCell tanımlandı
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
+        cell.userEmailLabel.text = "test"
+        cell.likeLabel.text = "0"
+        cell.commentLabel.text = "comment"
+        cell.userImageView.image = UIImage(named: "select.png")
+        return cell
+    }
 
 }
