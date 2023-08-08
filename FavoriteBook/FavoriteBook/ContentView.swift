d65a8e37aef54c076858c764efb98bc8747427c2
Ali Mert Kaya <mert_k4433@hotmail.com>
2023-08-08 14:33:04 +0300

    Navigation added

--- FavoriteBook/FavoriteBook/ContentView.swift
+++ FavoriteBook/FavoriteBook/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(myFavorites) { favorite in
+                    Section(header: Text(favorite.title)) {
+                        ForEach(favorite.elements) { element in
+                            NavigationLink(destination: DetailsView()) {
+                                Text(element.name)
+                            }
+                        }
+                    }
+                }
+            }.navigationBarTitle(Text("Favorite Book"))
         }
-        .padding()
     }
 }
 
