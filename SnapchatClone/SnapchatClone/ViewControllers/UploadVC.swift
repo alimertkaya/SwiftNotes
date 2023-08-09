adddf5ce2b798d88e0972ea17322253d8a7e9557
Ali Mert Kaya <mert_k4433@hotmail.com>
2023-08-09 12:38:11 +0300

    uploadClicked added

--- SnapchatClone/SnapchatClone/ViewControllers/UploadVC.swift
+++ SnapchatClone/SnapchatClone/ViewControllers/UploadVC.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import PhotosUI
+import FirebaseStorage
+import FirebaseFirestore
 
 class UploadVC: UIViewController, UIImagePickerControllerDelegate, PHPickerViewControllerDelegate{
 
@@ -43,5 +45,46 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate, PHPickerViewC
     }
     
     @IBAction func uploadClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = uploadImageView.image?.jpegData(compressionQuality: 0.5) {
+            let uuid = UUID().uuidString
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            
+            imageReference.putData(data) { metadata, error in
+                if error != nil {
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                }
+                else {
+                    imageReference.downloadURL { url, error in
+                        if error == nil {
+                            let imageUrl = url?.absoluteString
+                            let fireStore = Firestore.firestore()
+                            let snapDictionary = ["imageUrl" : imageUrl!, "snapOwner" : UserSingleton.sharedUserInfo.username, "date" : FieldValue.serverTimestamp()] as [String : Any]
+                            
+                            fireStore.collection("Snaps").addDocument(data: snapDictionary) { error in
+                                if error != nil {
+                                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                                }
+                                else {
+                                    self.tabBarController?.selectedIndex = 0
+                                    self.uploadImageView.image = UIImage(named: "select.png")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func makeAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
     }
 }
