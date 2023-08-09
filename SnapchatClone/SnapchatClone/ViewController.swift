962ac30e4c35a19324b06eda7b0c9086752db988
Ali Mert Kaya <mert_k4433@hotmail.com>
2023-08-09 10:05:21 +0300

    SnapchatClone created

---
+++ SnapchatClone/SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Ali Mert Kaya on 9.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
