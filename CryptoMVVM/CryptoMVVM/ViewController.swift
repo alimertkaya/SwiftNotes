36a4cb8405e25ca2d7708589cc64f41d8f624e0c
Ali Mert Kaya <mert_k4433@hotmail.com>
2023-08-11 11:20:22 +0300

    CryptoMVVM created

---
+++ CryptoMVVM/CryptoMVVM/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CryptoMVVM
+//
+//  Created by Ali Mert Kaya on 11.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell", for: indexPath) as! CryptoTableViewCell
+        return cell
+    }
+}
+
