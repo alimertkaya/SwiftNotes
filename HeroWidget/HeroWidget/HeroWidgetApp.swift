0631fd942f051ba86d8592200feb74abb3bece38
Ali Mert Kaya <mert_k4433@hotmail.com>
2023-08-14 14:02:08 +0300

    HeroWidget created

---
+++ HeroWidget/HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by Ali Mert Kaya on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
