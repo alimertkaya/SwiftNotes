27fb2b250c8ff2ebe0bf15f739d127f1e8fbb01c
Ali Mert Kaya <mert_k4433@hotmail.com>
2023-08-14 12:30:38 +0300

    View updated

--- CryptoViper/CryptoViper/View.swift
+++ CryptoViper/CryptoViper/View.swift
@@ -13,9 +13,68 @@ import UIKit
 // Viewcontroller
 
 protocol AnyView {
+    var presenter : AnyPresenter? {get set}
     
+    func update(with cryptos : [Crypto])
+    func update(with error : String)
 }
 
-class cryptoViewController : UIViewController, AnyView {
+class cryptoViewController : UIViewController, AnyView, UITableViewDelegate, UITableViewDataSource {
+    var presenter: AnyPresenter?
+    var cryptos : [Crypto] = []
     
+    private let tableView : UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.isHidden = true
+        return table
+    }()
+    
+    private let messageLabel : UILabel = {
+        let label = UILabel()
+        label.isHidden = false
+        label.text = "Downloading..."
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(tableView)
+        view.addSubview(messageLabel)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // Ekran boyutu kadar
+        tableView.frame = view.bounds
+        messageLabel.frame = CGRect(x: view.frame.width / 2 - 100, y: view.frame.height / 2 - 25, width: 200, height: 50)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cryptos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func update(with cryptos: [Crypto]) {
+        DispatchQueue.main.async {
+            self.cryptos = cryptos
+            self.messageLabel.isHidden = true
+            self.tableView.reloadData()
+            self.tableView.isHidden = false
+        }
+    }
+    
+    func update(with error: String) {
+            
+    }
 }
