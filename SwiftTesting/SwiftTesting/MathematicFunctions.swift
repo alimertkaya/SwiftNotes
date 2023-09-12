5e2c1c6cfd76f7fb97234a4af88c3ab23c61c977
Ali Mert Kaya <mert_k4433@hotmail.com>
2023-09-12 12:40:32 +0300

    UnitTest added

---
+++ SwiftTesting/SwiftTesting/MathematicFunctions.swift
@@ -0,0 +1,23 @@
+//
+//  MathematicFunctions.swift
+//  SwiftTesting
+//
+//  Created by Ali Mert Kaya on 12.09.2023.
+//
+
+import Foundation
+
+class MathematicFunctions {
+    
+    func addIntegers(x: Int, y: Int) -> Int {
+        return x + y
+    }
+    
+    func multiplyNumbers(x: Int, y: Int) -> Int {
+        return x * y
+    }
+    
+    func divideIntegers(x: Int, y: Int) -> Int {
+        return x / y
+    }
+}
---
+++ SwiftTesting/SwiftTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTesting
+//
+//  Created by Ali Mert Kaya on 12.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
